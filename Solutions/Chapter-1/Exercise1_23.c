d2d7b8b52796f3a3384d3a8a84fbfd56adb53f88
Manish Patel <manish.patel.mnnit@gmail.com>
2017-05-09 09:39:49 +0530

    Update Exercise1_23.c

--- Solutions/Chapter-1/Exercise1_23.c
+++ Solutions/Chapter-1/Exercise1_23.c
@@ -10,54 +10,70 @@
  */
 int main()
 {
-  int c,stateSin,stateMulti,seenSlash,seenStar;
+  int c,stateSin,stateMulti,seenSlash,seenStar,stateQuote;
   stateSin=OUT;//stateSin variable represents whether inside comment or not
   // 0 means out of comment
   stateMulti=OUT;
+  stateQuote=OUT;
   seenSlash=seenStar=0;// not seen hash symbol or star yet
   // seenStar tracks the ending star of multiline comment
   while((c=getchar())!=EOF)
   {
-    if(seenSlash==0 && c=='/' && stateMulti!=IN)
+    if(stateQuote==OUT && stateMulti==OUT && stateSin==OUT && c=='"') // inside quote
+    {
+      putchar(c);
+      stateQuote=IN;
+    }
+    else if(stateQuote==IN && stateMulti==OUT && stateSin==OUT && c=='"') // quote ends
+    {
+      putchar(c);
+      stateQuote=OUT;
+    }
+    else if(stateQuote==IN && stateMulti==OUT && stateSin==OUT)
+    {
+      putchar(c);
+    }
+    else if(stateQuote==OUT && seenSlash==0 && c=='/' && stateSin!=IN && stateMulti!=IN)
       seenSlash=1;
-    else if(seenSlash==1 && c=='/')// inside single line comment
+    else if(stateQuote==OUT && seenSlash==1 && c=='/')// inside single line comment
     {
       stateSin=IN; 
       seenSlash=0;
     }
-    else if(seenStar==1 && c=='/') // end of multiline comment
+    else if(stateQuote==OUT && seenStar==1 && c=='/') // end of multiline comment
     {
       stateMulti=OUT;
       seenStar=0;
     }
-    else if(seenSlash==1 && c=='*') // inside multi line comment
+    else if(stateQuote==OUT && seenSlash==1 && c=='*') // inside multi line comment
     {
       stateMulti=IN;
       seenSlash=0;	
     }
-    else if(c!='\n' && stateSin==IN)// inside single line, hence skip chars
+    else if(stateQuote==OUT && c!='\n' && stateSin==IN)// inside single line comm, hence skip chars
       ;
-    else if(c=='*' && stateMulti==IN) 
+    else if(stateQuote==OUT && c=='*' && stateMulti==IN) 
     {
       seenStar=1;  // this takes of the case that * should precede / without gap
     }
-    else if(stateMulti==IN)
+    else if(stateQuote==OUT && stateMulti==IN)
     {
       seenStar=0;
     } 
-    else 
+    else if(stateQuote==OUT)
     { 
       if(seenSlash==1)
       {
-	putchar('/');
+	putchar('/'); // this takes care of case like this-> / /
 	seenSlash=0;
       }
-      if(c=='\n' && stateSin==IN)
+      if(c=='\n' && stateSin==IN) // single line comment ends at newline
       {
 	stateSin=OUT;
       }
-	putchar(c);
+      putchar(c);
     }
   }
+  //printf(" /* */"); // Uncomment this to see quoting 
   return 0;
 }
