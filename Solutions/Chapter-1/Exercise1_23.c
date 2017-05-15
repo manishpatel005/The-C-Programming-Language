a5ca81bb3aede826706e27ea0cb7440dd1264038
Manish Patel <manish.patel.mnnit@gmail.com>
2017-05-15 11:19:17 +0530

    Update Exercise1_23.c

--- Solutions/Chapter-1/Exercise1_23.c
+++ Solutions/Chapter-1/Exercise1_23.c
@@ -1,79 +1,89 @@
 #include<stdio.h>
-#define OUT 0
-#define IN 1
-/* program to remove all comments from a C program
- * Multiline comments: slash star -star slash
- * Single line comment : slash slash
- * and Comments dont nest i.e inside multiline comment
- * you cant use the multiline comment symbol
- * You can run this program on itself :)
+/* Program to remove all comments from a C program. Don't forget to handle quoted strings
+ * and character constants properly
  */
-int main()
+// Run this program on itself :) 
+void skipSingleQuote(int prev)
 {
-  int c,stateSin,stateMulti,seenSlash,seenStar,stateQuote;
-  stateSin=OUT;//stateSin variable represents whether inside comment or not
-  // 0 means out of comment
-  stateMulti=OUT;
-  stateQuote=OUT;
-  seenSlash=seenStar=0;// not seen hash symbol or star yet
-  // seenStar tracks the ending star of multiline comment
-  while((c=getchar())!=EOF)
+   putchar(prev);
+   int c;
+   while((c=getchar())!=EOF)
+   {
+     if(prev=='\\' && c=='\'')
+       putchar(c);
+     else if(c=='\'')
+       break;
+     else 
+       putchar(c);
+     prev=c;
+   }
+   putchar(c);
+}
+void skipDoubleQuote(int c)
+{
+   putchar(c);
+   while((c=getchar())!=EOF && c!='"')
+   {
+     putchar(c);
+   }
+   putchar(c);
+}
+void skipComments(int isSingleLine)
+{
+  int c, prev=' ';
+  if(isSingleLine==1)
+  {  
+    while((c=getchar())!=EOF && c!='\n')
+    {
+      ; 
+    }
+    putchar(c); 
+  }
+  else // multiline
   {
-    if(stateQuote==OUT && stateMulti==OUT && stateSin==OUT && c=='"') // inside quote
+     while((c=getchar())!=EOF)
     {
-      putchar(c);
-      stateQuote=IN;
-    }
-    else if(stateQuote==IN && stateMulti==OUT && stateSin==OUT && c=='"') // quote ends
-    {
-      putchar(c);
-      stateQuote=OUT;
-    }
-    else if(stateQuote==IN && stateMulti==OUT && stateSin==OUT)
-    {
-      putchar(c);
-    }
-    else if(stateQuote==OUT && seenSlash==0 && c=='/' && stateSin!=IN && stateMulti!=IN)
-      seenSlash=1;
-    else if(stateQuote==OUT && seenSlash==1 && c=='/')// inside single line comment
-    {
-      stateSin=IN; 
-      seenSlash=0;
-    }
-    else if(stateQuote==OUT && seenStar==1 && c=='/') // end of multiline comment
-    {
-      stateMulti=OUT;
-      seenStar=0;
-    }
-    else if(stateQuote==OUT && seenSlash==1 && c=='*') // inside multi line comment
-    {
-      stateMulti=IN;
-      seenSlash=0;	
-    }
-    else if(stateQuote==OUT && c!='\n' && stateSin==IN)// inside single line comm, hence skip chars
-      ;
-    else if(stateQuote==OUT && c=='*' && stateMulti==IN) 
-    {
-      seenStar=1;  // this takes of the case that * should precede / without gap
-    }
-    else if(stateQuote==OUT && stateMulti==IN)
-    {
-      seenStar=0;
-    } 
-    else if(stateQuote==OUT)
-    { 
-      if(seenSlash==1)
-      {
-	putchar('/'); // this takes care of case like this-> / /
-	seenSlash=0;
-      }
-      if(c=='\n' && stateSin==IN) // single line comment ends at newline
-      {
-	stateSin=OUT;
-      }
-      putchar(c);
+      if(prev=='*' && c=='/')
+         break;
+      prev=c;
     }
   }
-  //printf(" /* */"); // Uncomment this to see quoting 
+}
+int main()
+{
+ 
+  int c,prev=' ';
+  while((c=getchar())!=EOF)
+  {
+   
+    if(prev=='/' && c=='/') // inside single line comment
+    {
+      skipComments(1);
+    }
+    else if(prev=='/' && c=='*') // inside multiline comment
+    {
+      skipComments(0);
+    }
+    else if(prev==' ' && c=='/')
+    {
+      ;
+    }
+    else if(c=='\'') // single quote
+    {
+       skipSingleQuote(c);
+    }
+    else if(c=='"')// inside quotes
+    { 
+      skipDoubleQuote(c);
+    }
+    else
+      putchar(c);
+
+    if(c=='\n')
+      prev=' ';
+    else
+      prev=c;
+  }
+  printf("/* */");
   return 0;
 }
